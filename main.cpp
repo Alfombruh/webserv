f76fdcf754e67a8558c0814a1c0562299d616364
Ionmi <ionmiromero@gmail.com>
2022-12-06 14:28:58 +0100

    [Version 0.3.0] (12-6-2022)

--- main.cpp
+++ main.cpp
@@ -4,17 +4,17 @@ int main(int argc, char **argv)
 {
 	if (argc != 2)
 		return (errorMessage("./webserv configFile.conf"));
-// start:
-	try
-	{
+	// start:
 		Server server(argv[1]);
 		server.setup();
 		server.run();
-	}
-	catch (const std::exception &e)
-	{
-		std::cerr << e.what() << '\n';
-		// goto start;
-	}
+	// try
+	// {
+	// }
+	// catch (const std::exception &e)
+	// {
+	// 	std::cerr << e.what() << '\n';
+	// 	goto start;
+	// }
 	return (0);
 }
